4cd4c0e0dfbff8b42d9b17fb4fa75d37a880e878
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2017-09-26 07:54:43 +1000

    feat(cartridge): Simplified cartridge reading for now

--- src/Emulator/Cartridge.hs
+++ src/Emulator/Cartridge.hs
@@ -46,45 +46,35 @@ parseHeader bs = INesFileHeader
 parseCart :: BS.ByteString -> ST s (Cartridge s)
 parseCart bs = do
   let header @ (INesFileHeader _ numPrg numChr control1 control2 _) = parseHeader bs
-      mapperType = (control2 .&. 0xF0) .|. (unsafeShiftR control1 4)
-      mirror     = (control1 .&. 1) .|. (shiftL ((unsafeShiftR control1 3) .&. 1) 1)
-      prgOffset  = numPrg * prgRomSize
-      chrOffset  = numChr * chrRomSize
-      chrRom     = if numChr == 0 then (BS.replicate chrRomSize 0)
-                   else sliceBS (headerSize + prgOffset) (headerSize + prgOffset + chrOffset) bs
-      prgRom     = sliceBS headerSize (headerSize + prgOffset) bs
-  chr <- VU.unsafeThaw $ VU.fromList $ BS.unpack chrRom
-  prg <- VU.unsafeThaw $ VU.fromList $ BS.unpack prgRom
-  sram <- VUM.replicate 0x2000 0
-  prgBanks <- newSTRef $ VUM.length prg `div` 0x4000
-  prgBank1 <- newSTRef $ 0
-  prgBank2 <- newSTRef $ 0 - 1
-  pure $ Cartridge header mirror prgBanks prgBank1 prgBank2 chr prg sram
+  let mirror = 1
+  let prgOffset  = numPrg * prgRomSize
+  let prgRom = sliceBS headerSize (headerSize + prgOffset) bs
+  let chrOffset = numChr * chrRomSize
+  let chrRom = if numChr == 0 then (BS.replicate chrRomSize 0)
+               else sliceBS (headerSize + prgOffset) (headerSize + prgOffset + chrOffset) bs
 
+  prg <- VU.unsafeThaw $ VU.fromList $ BS.unpack prgRom
+  chr <- VU.unsafeThaw $ VU.fromList $ BS.unpack chrRom
+  sram <- VUM.replicate 0x2000 0
+
+  pure $ Cartridge header mirror chr prg sram
 
 readCart :: Cartridge s -> Word16 -> ST s Word8
 readCart cart addr
   | addr' <  0x2000 = VUM.read (chrRom cart) addr'
-  | addr' >= 0xC000 = VUM.read (prgRom cart) ((prgBank2 * 0x4000) + (addr' - 0xC000))
-  | addr' >= 0x8000 = VUM.read (prgRom cart) ((prgBank1 * 0x4000) + (addr' - 0x8000))
+  | addr' >= 0x8000 = do
+    let index = (addr' - 0x8000 ) `mod` VUM.length (prgRom cart)
+    VUM.read (prgRom cart) index
   | addr' >= 0x6000 = VUM.read (prgRom cart) (addr' - 0x6000)
-  | otherwise = error $ "Erroneous mapper2 read detected!: " ++ prettifyWord16 addr
-  where
-    addr' = fromIntegral addr
-    prgBanks = VUM.length (prgRom cart) `div` 0x4000
-    prgBank1 = 0
-    prgBank2 = prgBanks - 1
+  | otherwise = error $ "Erroneous cart read detected!: " ++ prettifyWord16 addr
+  where addr' = fromIntegral addr
 
 writeCart :: Cartridge s -> Word16 -> Word8 -> ST s ()
 writeCart cart addr v
   | addr' < 0x2000 = VUM.write (chrRom cart) addr' v
-  | addr' >= 0x8000 = do
-    prgBanks <- readSTRef (prgBanks cart)
-    modifySTRef (prgBank1 cart) (const $ toInt v `mod` prgBanks)
   | addr' >= 0x6000 = VUM.write (sRam cart) (addr' - 0x6000) v
-  | otherwise = error $ "Erroneous mapper2 write detected!" ++ prettifyWord16 addr
-  where
-    addr' = fromIntegral addr
+  | otherwise = error $ "Erroneous cart write detected!" ++ prettifyWord16 addr
+  where addr' = fromIntegral addr
 
 headerSize :: Int
 headerSize = 0x10
